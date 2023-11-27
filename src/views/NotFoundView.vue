08dd100a0d9e5e96e77448f81ea800539771f4cc
focunti-renzorh <renzo.rojas@focunti.com>
2023-11-27 09:51:23 -0500

    NotFoundView modificado

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -3,11 +3,15 @@
 </script>
 
 <template>
-<div>
-  Not Found View
+<div id="container" class="h-80 w-full bg-white rounded-2xl flex flex-col justify-center items-center gap-16">
+  <span class="text-gray-900 text-9xl font-semibold">Ups!</span>
+  <span class="text-gray-900 text-3xl font-semibold">No hemos podido ubicar la pagina a la que deseas acceder</span>
 </div>
 </template>
 
 <style scoped>
+#container{
+  background-image: url("../assets/images/404.png");
+}
 
 </style>
